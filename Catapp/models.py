db535b114a20e653158dc63e9bda21379685dda8
Caio Dias Costa <lowbit100@gmail.com>
2025-09-13 17:04:47 -0300

    [refactor]: mudei um pouco o funcionamento do banco para se adequar a API

--- Catapp/models.py
+++ Catapp/models.py
@@ -9,14 +9,21 @@ class Dono(models.Model):
         return self.nome + "email: " + self.email
     
 class Gato(models.Model):
+    api_id = models.CharField(
+        max_length=100,
+        unique= True,
+        verbose_name="ID da API")
+
     raca = models.CharField(max_length=25)
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=500)
+
     foto_gato = models.URLField()
 
-    dono = models.ForeignKey(Dono, on_delete=models.CASCADE, related_name="gatos")
+    dono = models.ForeignKey(
+        Dono, 
+        on_delete=models.CASCADE, 
+        related_name="dono",
+        verbose_name= "dono")
 
     def __str__(self):
-        if self.raca:
-            return self.raca
-        else:
-            return "sem raça"
+        return self.raca
