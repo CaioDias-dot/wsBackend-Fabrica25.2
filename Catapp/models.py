f45093b9a0550fbbc6e9693707e2f1138f928ebc
Caio Dias Costa <lowbit100@gmail.com>
2025-09-14 11:10:47 -0300

    [refactory]: mudei os campos de Dono

--- Catapp/models.py
+++ Catapp/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Dono(models.Model):
+    user = models.OneToOneField(User, 
+        on_delete=models.CASCADE, 
+        related_name='dono_perfil',
+        null = True,
+        blank=True)
+
     nome = models.CharField(max_length=100)
     email = models.EmailField(unique = True)
-    endereco = models.CharField(max_length=120)
+    senha = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nome + " email: " + self.email
