4ef5c7aa5a485c8e7ffee3aa48c984106140e5fa
Caio Dias Costa <lowbit100@gmail.com>
2025-09-14 11:12:47 -0300

    [refactory]: mudança para acompanhar a solução do erro de login

--- Catapp/forms.py
+++ Catapp/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-from .models import Dono
+from .models import Dono,Gato
 
 class DonoForm(forms.ModelForm):
     class Meta:
         model = Dono
-        fields = ['nome', 'email', 'endereco']
+        fields = ['nome', 'email', 'senha']
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(label="Email")
+    senha = forms.CharField(label="Senha", widget=forms.PasswordInput)
+
+class GatoForm(forms.ModelForm):
+    class Meta:
+        model = Gato
+        fields = ['api_id','raca','descricao','foto_gato','dono']
